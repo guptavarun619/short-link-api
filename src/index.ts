e0f9f0bda4ce04ce000802876b4dde7770e3e165
Varun Vaibhav <guptavarun619@gmail.com>
2023-06-12 22:05:35 +0530

    feat: Added body parser for reading req body

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import { PORT } from './config/serverConfig';
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/hello', (req, res) => {
     res.send("Hello from the server");
