ea80d0b388aa63bff09f997f4a82bf65aafab1b1
Varun Vaibhav <guptavarun619@gmail.com>
2023-06-12 22:06:14 +0530

    feat: Added JWT_SECRET_KEY env variable

--- src/config/serverConfig.ts
+++ src/config/serverConfig.ts
@@ -4,3 +4,4 @@ dotenv.config();
 export const PORT = process.env.PORT;
 export const MONGO_DB_URI = process.env.MONGO_DB_URI;
 export const SALT_ROUND = process.env.SALT_ROUND;
+export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
